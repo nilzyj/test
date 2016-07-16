d05a3bc6677d8637755304cfffba34c9f230ce6b
nilzyj <nilzyj@163.com>
2016-07-16 16:13:32 +0800

    save

---
+++ Android/WMessage/myapplication/src/main/java/com/bbk/zyj/myapplication/message.java
@@ -0,0 +1,25 @@
+package com.bbk.zyj.myapplication;
+
+/**
+ * Created by Administrator on 2016/7/16.
+ */
+public class message {
+    private String mName;
+    private String mContent;
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getContent() {
+        return mContent;
+    }
+
+    public void setContent(String content) {
+        mContent = content;
+    }
+}
