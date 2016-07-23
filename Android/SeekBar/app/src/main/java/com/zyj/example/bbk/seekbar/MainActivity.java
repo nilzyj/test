c47a2175e6a02d4b3eb11d82bee370e41fd79aad
nilzyj <nilzyj@163.com>
2016-07-23 15:04:53 +0800

    add SeekBar

---
+++ Android/SeekBar/app/src/main/java/com/zyj/example/bbk/seekbar/MainActivity.java
@@ -0,0 +1,41 @@
+package com.zyj.example.bbk.seekbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekBar;
+    private TextView tv1;
+    private TextView tv2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekBar=(SeekBar) findViewById(R.id.seekBar);
+        tv1=(TextView) findViewById(R.id.tv1);
+        tv2=(TextView) findViewById(R.id.tv2);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            //数值改变
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tv1.setText("正在拖动");
+                tv2.setText("当前数值：" + i);
+            }
+            //开始拖动
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                tv1.setText("开始拖动");
+            }
+            //停止拖动
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                tv1.setText("停止拖动");
+            }
+        });
+    }
+}
