02ce6227c088bdcf825ebfd4be3e509c6a170db5
nilzyj <nilzyj@163.com>
2017-04-17 22:30:11 +0800

    update

---
+++ Android/RSSDemo/app/src/main/java/com/example/zyj/rssdemo/RSSItem.java
@@ -0,0 +1,14 @@
+package com.example.zyj.rssdemo;
+
+/**
+ * Created by dim on 2016/8/17.
+ */
+public class RSSItem {
+    public static final String TITLE = "titel";
+    public static final String PUBDATE = "pubdate";
+    private String title = null;
+    private String description = null;
+    private String link = null;
+    private String category = null;
+    private String pubdate = null;
+}
