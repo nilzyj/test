b57fb3ca1a4c475418ad3f9914f7b40a00f07747
nilzyj <nilzyj@163.com>
2017-04-17 23:11:07 +0800

    update

--- Android/custom-button/app/src/main/java/com/dim/custom_button/MainActivity.java
+++ Android/custom-button/app/src/main/java/com/dim/custom_button/MainActivity.java
@@ -1,13 +1,14 @@
 package com.dim.custom_button;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+//效果如图drawable/custom_button.png
+public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 }
