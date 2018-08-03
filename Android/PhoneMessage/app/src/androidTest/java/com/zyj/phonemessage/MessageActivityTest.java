98839e4d6fad75d9b1bf3a9c8f57839062cbf41d
nilzyj <nilzyj@163.com>
2018-08-03 10:54:32 +0800

    del android

--- Android/PhoneMessage/app/src/androidTest/java/com/zyj/phonemessage/MessageActivityTest.java
+++
@@ -1,7 +0,0 @@
-package com.zyj.phonemessage;
-
-/**
- * Created by Administrator on 2016/8/15.
- */
-public class MessageActivityTest{
-}
--- Android/WMessage/contact/src/main/java/com/bbk/zyj/Contact.java
+++
@@ -1,8 +0,0 @@
-package com.bbk.zyj;
-
-/**
- * Created by Administrator on 2016/7/22.
- */
-public class Contact {
-
-}
